c510bc2be1e67fe786d607ff3465a93b4a2afe7d
lord-amresh <amresh2006@gmail.com>
2025-05-11 09:22:06 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<h1>This is home page</h1>}/>
+      <Route path="/about" element={<h2>This is about page</h2>}/>
+      <Route path="/contact" element={<div>
+        <h2>This is a contact page</h2>
+        <p>here will be our contact description</p>
+        <p>Contact No : 9818062229</p>
+      </div>}/>
+    </Routes>
+    </BrowserRouter>
+
+  )
+}
+
+// component vaneko type of function jasle jaile ni JSX return garxa (JSX = html jasto syntax)
+
+export default App
