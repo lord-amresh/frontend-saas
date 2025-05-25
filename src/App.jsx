d22b0a97a972b2467b600057e1bab4cd800ada45
lord-amresh <amresh2006@gmail.com>
2025-05-25 16:15:51 +0545

    making pages

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
         <h2>This is a contact page</h2>
         <p>here will be our contact description</p>
         <p>Contact No : 9818062229</p>
+        <a href="/about">Go to About Page</a>
       </div>}/>
     </Routes>
     </BrowserRouter>
